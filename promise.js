cbbb40c531e0aa5829bb26525846538714964127
jsPeasant <zengkang5618@163.com>
2025-03-04 14:46:12 +0800

    feat: shuffle algorithm

--- promise.js
+++
@@ -1,9 +0,0 @@
-class MyPromise {
-    constructor(exector) {
-        this.state = 'pending';
-        this.value = undefined;
-        this.error = undefined;
-        this.resolveCallbacks = [];
-        this.rejectCallbacks = [];
-    }
-}
